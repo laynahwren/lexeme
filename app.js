962a7270485be3273baa490bb87869ed631ceeee
Laynah Varnum <laynah.varnum98@gmail.com>
2023-04-10 18:07:59 -0500

    add get route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express()
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
