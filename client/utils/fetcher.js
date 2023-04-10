b909cb274201e3842851c7cfd9d2f65b25fcd226
Laynah Varnum <laynah.varnum98@gmail.com>
2023-04-09 20:28:02 -0500

    add fetch routes for apis

---
+++ client/utils/fetcher.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const KEY = process.env.API_KEY
+
+// Get book data
+export const fetchBook = (params) => {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${params}&key=${KEY}`)
+        .then(function (response) {
+            console.log(response.data)
+        })
+}
+
+// Get word
+export const fetchWord = (params) => {
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${params}`)
+        .then(function (response) {
+            console.log(response.data)
+        })
+}
